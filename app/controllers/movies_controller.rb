156c1e11bf3a68b00228184023d508ac5cf4c08c
kylelyk <kylelyk@gmail.com>
2013-10-12 12:01:43 -0700

    Ready for Heroku

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,42 @@
 class MoviesController < ApplicationController
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
+    id = params[:id] # retrieve movie ID from URI route  
+    begin 
+      @movie = Movie.find(id) # look up movie by unique ID
+    rescue
+      notice = "Movie with id #{id} could not be found."
+      flash.keep
+      
+      redirect_to movies_path
+      return
+    end
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
-    @movies = Movie.all
+    #restore the ratings settings if all are unchecked
+    if(params[:ratings] == {})
+      params[:ratings] = session[:ratings]
+    end
+    
+    #overwrite all session parameters that were passed in params
+    params.each {|elt, value| session[elt] = value}
+
+    @all_ratings = Movie.ratings
+    
+    #sorting by title or release
+    if(session.has_key?(:sorting))
+    	@movies = Movie.order("#{session[:sorting]}")
+    else
+	    @movies = Movie.all
+    end
+
+    #filtering by rating
+    if(session.has_key?(:ratings))
+      @ratings = session[:ratings].keys
+      @movies = @movies.keep_if{|elem| @ratings.include?(elem.rating)}
+    end
   end
 
   def new
@@ -15,6 +44,7 @@ class MoviesController < ApplicationController
   end
 
   def create
+    #debugger
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
@@ -37,5 +67,4 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
 end
